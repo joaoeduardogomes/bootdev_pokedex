42798a93e1f74644553bf51d89194c22099ad9ac
João Gomes <joaoeduardo.gomes21@gmail.com>
2026-02-23 20:42:05 -0300

    create startREPL function

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,32 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
 export function cleanInput(text: string): string[] {
     const clearText = text.trim();
     const lowerText = clearText.toLowerCase();
     const splittedText = lowerText.split(" ");
 
     return splittedText.filter(text => text.length > 0);
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: stdin,
+        output: stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt()
+
+    rl.on("line", (line) => {
+        const arrWords = cleanInput(line);
+
+        if (arrWords.length === 0) {
+            rl.prompt();
+            return;
+        }
+
+        console.log(`Your command was: ${arrWords[0]}`);
+        rl.prompt();
+    })
 }
